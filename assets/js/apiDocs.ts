a63f7b2dc0e37fbafa744d84534fb8e43a97d0c9
Damian Peralta <damianperaltam@gmail.com>
2021-07-05 16:42:01 +0200

    Renaming and organizing petstore in api-docs

--- assets/js/apiDocs.ts
+++ assets/js/apiDocs.ts
@@ -1,13 +1,13 @@
 import SwaggerUI from 'swagger-ui'
 
 document.addEventListener('DOMContentLoaded', function () {
-  const container = document.getElementById('swagger-ui-wrapper')
+  const container = document.getElementById('api-docs')
   if (!container) {
     return undefined
   }
 
   SwaggerUI({
-    dom_id: '#swagger-ui-wrapper',
+    dom_id: '#api-docs',
     url: container.dataset.url
   })
 })
--- content/docs/petstore.md
+++ content/docs/petstore.md
@@ -1,9 +1,9 @@
 ---
-title: "Oas Swagger Ui"
+title: "Pet Store"
 date: 2021-06-18T12:48:06+02:00
 draft: false
 ---
 
-## Displaying a OAS v3 with Swagger UI
+## Displaying the Pet Store API with Swagger UI
 
 {{< swagger-ui "https://petstore3.swagger.io/api/v3/openapi.json">}}
