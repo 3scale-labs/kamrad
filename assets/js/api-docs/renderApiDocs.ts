c8753515b0dec84a69dc26f3f6b8bf51428566ab
dd di cesare <didi@posteo.net>
2021-08-18 22:04:51 +0700

    [apiDocs] Making apiDocs accepting both the spec JSON object and url

--- assets/js/api-docs/renderApiDocs.ts
+++ assets/js/api-docs/renderApiDocs.ts
@@ -1,10 +1,15 @@
 import SwaggerUI from 'swagger-ui'
 
-const renderApiDocs = (containerId: string, url: string ) => (
-  SwaggerUI({
+const urlExpression = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
+const urlRegex = new RegExp(urlExpression)
+
+const renderApiDocs = (containerId: string, spec: string ) => {
+  const specSource = !!spec.match(urlRegex) ? 'url' : 'spec'
+  const swaggerOptions = {
     dom_id: `#${containerId}`,
-    url: url
-  })
-)
+    [specSource]: JSON.parse(spec)
+  }
+  SwaggerUI(swaggerOptions)
+}
 
 export { renderApiDocs }
--- assets/js/apiDocs.ts
+++ assets/js/apiDocs.ts
@@ -3,6 +3,6 @@ import { renderApiDocs } from "./api-docs/renderApiDocs"
 document.addEventListener('DOMContentLoaded', function () {
   Array.from(document.getElementsByClassName('api-docs'))
     .forEach((node: HTMLElement) =>
-      renderApiDocs(node.id, node.dataset.url)
+      renderApiDocs(node.id, node.dataset.spec)
     )
 })
